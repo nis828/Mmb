d1b65547a54febe0437af149f322ef25562c0e90
nis <niranjanchembra3@gmail.com>
2025-10-16 11:50:48 +0530

    Rename bot from 'Ultimate Music Bot' to 'Sopanam Music Bot'

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 /**
- * Ultimate Music Bot 
+ * Sopanam Music Bot 
  * Comprehensive Discord Bot
  * 
  * @fileoverview Core application
  * @version 1.0.0
- * @author GlaceYT
+ * @author nis
  */
 
 const DiscordClientFramework = require('discord.js').Client;
@@ -374,4 +374,5 @@ enterpriseApplicationManager.executeApplicationBootstrap();
 
 
 module.exports = enterpriseApplicationManager.clientRuntimeInstance;
+
 shiva.initialize(enterpriseApplicationManager.clientRuntimeInstance);
